6fbc35fc3018b2aa564eb8b4900dd0997fbf6dc8
iwanbazz <iwan.bazz@gmail.com>
2018-11-05 12:06:17 +0700

    Remove unneeded item

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Kerja Praktek.</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://rentalmobilpekanbaru.id"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Rental Mobil Pekanbaru
           </a>
         </header>
       </div>
