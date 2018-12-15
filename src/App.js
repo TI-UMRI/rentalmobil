9bff497951eec9ee4067ea4701079bb498bc3fb9
iwanbazz <iwan.bazz@gmail.com>
2018-12-16 02:09:44 +0700

    Added component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import React, { Component } from "react";
 
+import SearchAppBar from "./component/AppBar";
+
 const appStyle = {
   textAlign: "center"
-};
-
-const appHeader = {
-  minHeight: "100vh",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  fontSize: "calc(10px + 2vmin)",
-  color: "white"
-};
-
-const appLink = {
-  color: "#047d9f"
 };
 
 class App extends Component {
   render() {
     return (
       <div style={appStyle}>
-        <header style={appHeader}>
-          <a
-            style={appLink}
-            href="https://rentalmobilpekanbaru.id"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Rental Mobil
-          </a>
-        </header>
+        <SearchAppBar />
       </div>
     );
   }
